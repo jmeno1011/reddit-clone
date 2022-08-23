0cb8082be3b7aa9b3fe70942b3c3b97f8ac92cc6
jmeno1011 <jwhltn8282@gmail.com>
2022-08-24 01:05:42 +0900

    db 연결

--- nextjs-server/src/data-source.ts
+++ nextjs-server/src/data-source.ts
@@ -1,14 +1,13 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { User } from "./entities/User";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: "localhost",
   port: 5432,
-  username: "test",
-  password: "test",
-  database: "test",
+  username: "postgres",
+  password: "z12512412!",
+  database: "postgres",
   synchronize: true,
   logging: false,
   entities: ["src/entities/**/*.ts"],
--- nextjs-server/src/server.ts
+++ nextjs-server/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import { AppDataSource } from "./data-source";
 
 const app = express();
 
@@ -11,4 +12,10 @@ app.get("/", (_, res) => res.send("running"));
 let port = 4000;
 app.listen(port, async () => {
   console.log(`Server running at http://localhost:${port}`);
+
+  AppDataSource.initialize()
+    .then(async () => {
+      console.log("database initialized");
+    })
+    .catch((error) => console.log(error));
 });
