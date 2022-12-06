df87ddf1b3b7a406382880b7cb83021494286183
menomik1011 <39692905+meno1011@users.noreply.github.com>
2022-12-06 10:52:38 +0900

    add description

--- nextjs-server/src/entities/Sub.ts
+++ nextjs-server/src/entities/Sub.ts
@@ -1,5 +1,6 @@
 import { Expose } from "class-transformer";
-import { BaseEntity, Column, Entity, Index, JoinColumn, ManyToOne, OneToMany } from "typeorm";
+import { Column, Entity, Index, JoinColumn, ManyToOne, OneToMany } from "typeorm";
+import BaseEntity from './Entity';
 import User from "./User";
 
 @Entity("subs")
@@ -24,9 +25,14 @@ export default class Sub extends BaseEntity {
     username: string;
 
     @ManyToOne(() => User)
+    // joincolumn을 이용해서 어떤 관계들이 왜래키를 가지고 있는지 나타냄
+    // name: 외래키 속성명(Sub테이블에서 외래키가 무엇인지 24번째 줄에 username임)
+    // referencedColumnName: 참조 엔티티의 참조 속성명이다. 참조되는 User의 username을 적어준다. 
     @JoinColumn({ name: 'username', referencedColumnName: 'username' })
     user: User;
 
+    // 데코레이터의 경우 OneToOne은 필수이지만 아래와 같은 
+    // OneToMany나 ManyToOne은 선택이다.
     @OneToMany(() => Post, (post) => post.sub)
     posts: Post[]
 
