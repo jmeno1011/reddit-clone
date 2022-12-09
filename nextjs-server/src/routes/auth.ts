8d672c53dc0051e6457b8d2c662544b0ec63ba50
jmeno1011 <jwhltn8282@gmail.com>
2022-12-09 22:01:07 +0900

    add login page, add auth.ts error process

--- nextjs-server/src/routes/auth.ts
+++ nextjs-server/src/routes/auth.ts
@@ -2,6 +2,13 @@ import { validate } from "class-validator";
 import { Request, Response, Router } from "express";
 import User from "../entities/User";
 
+const mapError = (errors: Object[]) => {
+    return errors.reduce((prev: any, err: any) => {
+        prev[err.property] = Object.entries(err.constraints[0][1]);
+        return prev;
+    }, {})
+}
+
 const register = async (req: Request, res: Response) => {
     const { email, username, password } = req.body;
 
@@ -26,7 +33,9 @@ const register = async (req: Request, res: Response) => {
         user.password = password;
 
         // 엔티티에 정해 놓은 조건으로 user 데이터의 유효성 검사를 해줌
-        errors = await validate(user)
+        errors = await validate(user);
+
+        if (errors.length > 0) return res.status(400).json(mapError(errors))
 
         // 유저 정보를 user table에 저장
         await user.save();
