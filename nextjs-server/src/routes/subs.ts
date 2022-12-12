72ee3eb748723d96b9858e38440a67cdd60fddd7
jmeno1011 <jwhltn8282@gmail.com>
2022-12-13 00:38:49 +0900

    add sub content

--- nextjs-server/src/routes/subs.ts
+++ nextjs-server/src/routes/subs.ts
@@ -12,16 +12,39 @@ const router = Router()
 const createSub = async (req: Request, res: Response, next) => {
     const { name, title, description } = req.body;
 
-    try{
+    try {
         let errors: any = {}
-        if(isEmpty(name)) errors.name = "이름은 비워둘 수 없습니다."
-        if(isEmpty(title)) errors.title = "제목은 비워둘 수 없습니다."
+        if (isEmpty(name)) errors.name = "이름은 비워둘 수 없습니다."
+        if (isEmpty(title)) errors.title = "제목은 비워둘 수 없습니다."
 
         const sub = await getRepository(Sub)
-        .createQueryBuilder()
-        
-    }catch(error){
+            .createQueryBuilder("sub")
+            .where("lower(sub.name) = :name", { name: name.toLowerCase() })
+            .getOne();
 
+        if (sub) errors.name = "서브가 이미 존재합니다."
+        if (Object.keys(errors).length > 0) {
+            throw errors;
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({error:"문제가 발생했습니다."})
+    }
+
+    try{
+        const user:User = res.locals.user;
+
+        const sub = new Sub()
+        sub.name = name;
+        sub.description = description;
+        sub.title = title;
+        sub.user = user;
+
+        await sub.save()
+        return res.json(sub)
+    }catch(error){
+        console.error(error);
+        return res.status(500).json({error:"문제가 발생했습니다."})
     }
     // 유저 정보가 있다면 sub 이름과 제목이 이미 있는 것인지 체크
 
