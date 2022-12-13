f8dd6a393f2e0681d1f1a49a74a3f14838510b79
jmeno1011 <jwhltn8282@gmail.com>
2022-12-13 23:55:02 +0900

    add nav bar

--- nextjs-app/src/pages/_app.tsx
+++ nextjs-app/src/pages/_app.tsx
@@ -2,12 +2,22 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import axios from 'axios'
 import { AuthProvider } from '../context/auth';
+import { useRouter } from 'next/router';
+import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASEURL + "/api";
   axios.defaults.withCredentials = true
+  const { pathname } = useRouter()
+  const authRoutes = ["/register", "/login"];
+  const authRoute = authRoutes.includes(pathname);
   return <AuthProvider>
-    <Component {...pageProps} />
+    {
+      !authRoute && <Navbar />
+    }
+    <div className={authRoute ? "" : "pt-12"}>
+      <Component {...pageProps} />
+    </div>
   </AuthProvider>
 }
 
