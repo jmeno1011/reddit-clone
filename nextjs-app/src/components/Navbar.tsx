d6cb80e6796ba74fd618faa71bb61f643d555e8f
jmeno1011 <jwhltn8282@gmail.com>
2022-12-14 00:37:53 +0900

    error login page

--- nextjs-app/src/components/Navbar.tsx
+++ nextjs-app/src/components/Navbar.tsx
@@ -1,9 +1,21 @@
+import axios from 'axios';
 import Link from 'next/link'
 import React from 'react'
-import { useAuthState } from '../context/auth'
+import { useAuthDispatch, useAuthState } from '../context/auth'
 
 const Navbar: React.FC = () => {
     const { loading, authenticated } = useAuthState();
+    const dispatch = useAuthDispatch();
+    const handleLogout = () => {
+        axios.post("/auth/logout")
+            .then(() => {
+                dispatch("LOGOUT");
+                window.location.reload();
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
     return (
         <div className='fixed inset-x-0 top-0 z-10 flex items-center justify-between h-16 px-5 bg-white'>
             <span className='text-2xl font-semibold text-gray-400'>
@@ -19,7 +31,7 @@ const Navbar: React.FC = () => {
             </div>
             <div className='flex'>
                 {!loading && (
-                    authenticated ? (<button className='w-20 p-2 mr-2 text-center text-white bg-gray-400 rounded'>로그아웃</button>
+                    authenticated ? (<button className='w-20 p-2 mr-2 text-center text-white bg-gray-400 rounded' onClick={handleLogout}>로그아웃</button>
                     ) : (
                         <>
                             <Link href={"/login"}><a className='w-20 p-2 mr-2 text-center text-blue-500 border-blue-500 rounded'>로그인</a></Link>
--- nextjs-server/src/routes/auth.ts
+++ nextjs-server/src/routes/auth.ts
@@ -100,10 +100,24 @@ const login = async (req: Request, res: Response) => {
     }
 }
 
+const logout = async (_: Request, res: Response) => {
+    res.set(
+        "Set-Cookie",
+        cookie.serialize("token", "", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            expires: new Date(0),
+            path: "/"
+        })
+    )
+}
+
 const router = Router();
 
 router.get("/me", userMiddleware, authMiddleware, me);
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", userMiddleware, authMiddleware, logout);
 
 export default router;
