ae72c9eb224e7a8683f8f3e04525722963a012f8
jmeno1011 <jwhltn8282@gmail.com>
2022-12-14 14:52:24 +0900

    add swr and add sub/topSub api

--- nextjs-server/src/routes/subs.ts
+++ nextjs-server/src/routes/subs.ts
@@ -6,6 +6,8 @@ import authMiddleware from "../middlewares/auth";
 import { isEmpty } from "class-validator";
 import { getRepository } from "typeorm";
 import Sub from "../entities/Sub";
+import { AppDataSource } from "../data-source";
+import Post from "../entities/Post";
 
 const router = Router()
 
@@ -28,11 +30,11 @@ const createSub = async (req: Request, res: Response, next) => {
         }
     } catch (error) {
         console.error(error);
-        return res.status(500).json({error:"문제가 발생했습니다."})
+        return res.status(500).json({ error: "문제가 발생했습니다." })
     }
 
-    try{
-        const user:User = res.locals.user;
+    try {
+        const user: User = res.locals.user;
 
         const sub = new Sub()
         sub.name = name;
@@ -42,9 +44,9 @@ const createSub = async (req: Request, res: Response, next) => {
 
         await sub.save()
         return res.json(sub)
-    }catch(error){
+    } catch (error) {
         console.error(error);
-        return res.status(500).json({error:"문제가 발생했습니다."})
+        return res.status(500).json({ error: "문제가 발생했습니다." })
     }
     // 유저 정보가 있다면 sub 이름과 제목이 이미 있는 것인지 체크
 
@@ -53,7 +55,28 @@ const createSub = async (req: Request, res: Response, next) => {
     // 저장한 정보 프론트엔드로 전달해주기
 }
 
+const topSubs = async (req: Request, res: Response) => {
+    try {
+        const imageUrlExp = `COALESCE(s."imageUrn", "https://www.gravater.com/avatar?d=mp&f=y")`;
+        const subs = await AppDataSource
+            .createQueryBuilder()
+            .select(`s.title, s.name, ${imageUrlExp} as "imageUrl", count(p.id) as "postCount"`)
+            .from(Sub, "s")
+            .leftJoin(Post, "p", `s.name = p.subName`)
+            .groupBy('s.title, s.name, "imageUrl"')
+            .orderBy(`"postCount"`, "DESC")
+            .limit(5)
+            .execute();
+
+        return res.json(subs)
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Something went wrong" });
+    }
+}
+
 
 router.post("/", userMiddleware, authMiddleware, createSub);
+router.get("/sub/topSubs", topSubs);
 
 export default router;
