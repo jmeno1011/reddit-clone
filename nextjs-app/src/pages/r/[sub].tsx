aad6a6787303a9355fb60b3dc70cafabae05c0e7
jmeno1011 <jwhltn8282@gmail.com>
2022-12-14 16:35:00 +0900

    add detail page ui

--- nextjs-app/src/pages/r/[sub].tsx
+++ nextjs-app/src/pages/r/[sub].tsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React from 'react'
 import useSWR from "swr"
@@ -14,9 +15,44 @@ const SubPage = () => {
     }
     const router = useRouter()
     const subName = router.query.sub;
-    const { data: sub, error } = useSWR(subName ? `/sub/${subName}` : null, fetcher)
+    const { data: sub, error } = useSWR(subName ? `/subs/${subName}` : null, fetcher)
     return (
-        <div>SubPage</div>
+        <>
+            {sub &&
+                <>
+                    <div>
+                        {/* 배너 이미지 */}
+                        <div className='bg-gray-400'>
+                            {sub.bannerUrl ? (
+                                <div className='h-56' style={{ backgroundImage: `url(${sub.bannerUrl})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+                            ) : (
+                                <div className='h-20 bg-gray-400'></div>
+                            )}
+                        </div>
+                        {/* 커뮤니티 메타 데이터 */}
+                        <div>
+                            <div className='h-20 bg-white'>
+                                <div className='relative flex max-w-5xl px-5 mx-auto'>
+                                    <div className='absolute' style={{ top: -15 }}>
+                                        {sub.imageUrl && (<Image src={sub.imageUrl} alt="커뮤니티 이미지" width={70} height={70} className="rounded-full" />)}
+                                    </div>
+                                </div>
+                                <div className='pt-1 pl-24'>
+                                    <div className='flex items-center'>
+                                        <h1 className='text-3xl font-bold'>{sub.title}</h1>
+                                    </div>
+                                </div>
+                                <p className='text-sm font-bold text-gray-400'>
+                                    /r/${sub.name}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    {/* 포스트와 사이드바 */}
+                    <div className='flex max-w-5xl px-4 pt-5 mx-auto'></div>
+                </>
+            }
+        </>
     )
 }
 
