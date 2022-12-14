a9f1e45009700fb02ff62d83f1dd640fc437c0c7
jmeno1011 <jwhltn8282@gmail.com>
2022-12-14 21:57:21 +0900

    fixed imageurl by add instanceToPlain

--- nextjs-server/src/entities/Entity.ts
+++ nextjs-server/src/entities/Entity.ts
@@ -1,3 +1,4 @@
+import { instanceToPlain } from "class-transformer";
 import { BaseEntity, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 // BaseEntity.d.ts에 있는 메소드들 사용
@@ -10,4 +11,10 @@ export default abstract class Entity extends BaseEntity{
 
     @UpdateDateColumn()
     updatedAt: Date;
+    
+    // class transformer 이용
+    toJSON(){
+        // Converts class (constructor) object to plain (literal) object. Also works with arrays.
+        return instanceToPlain(false)
+    }
 }
