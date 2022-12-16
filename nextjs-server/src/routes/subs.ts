2085a38d5953c8c3241292bbe899eb69790781bb
jmeno1011 <jwhltn8282@gmail.com>
2022-12-16 21:34:07 +0900

    upload method done

--- nextjs-server/src/routes/subs.ts
+++ nextjs-server/src/routes/subs.ts
@@ -9,6 +9,7 @@ import Post from "../entities/Post";
 import multer, { FileFilterCallback } from "multer";
 import { makeId } from "../utils/helpers";
 import path from "path";
+import { unlinkSync } from "fs";
 
 const router = Router()
 
@@ -86,43 +87,87 @@ const topSubs = async (req: Request, res: Response) => {
     }
 }
 
-const onwSub = async (req:Request, res:Response, next:NextFunction)=>{
-    const user:User = res.locals.user;
+const ownSub = async (req: Request, res: Response, next: NextFunction) => {
+    const user: User = res.locals.user;
     try {
-        const sub = await Sub.findOneOrFail({where:{name:req.params.name}});
+        const sub = await Sub.findOneOrFail({ where: { name: req.params.name } });
 
-        if(sub.username !== user.username){
-            return res.status(403).json({error:"이 커뮤니티를 소유하고 있지 않습니다."});
+        if (sub.username !== user.username) {
+            return res.status(403).json({ error: "이 커뮤니티를 소유하고 있지 않습니다." });
         }
         res.locals.sub = sub;
         next();
     } catch (error) {
         console.error(error);
-        return res.status(500).json({error:"문제가 발생했습니다."})
-        
+        return res.status(500).json({ error: "문제가 발생했습니다." })
+
     }
 }
 
 const upload = multer({
     storage: multer.diskStorage({
-        destination:"public/image",
-        filename:(_, file, callback)=>{
+        destination: "public/image",
+        filename: (_, file, callback) => {
             const name = makeId(10);
-            callback(null, name+ path.extname(file.originalname))
+            callback(null, name + path.extname(file.originalname))
         },
     }),
-    fileFilter:(_, file: any, callback: FileFilterCallback)=>{
-        if(file.mimetype === "image/jpeg" || file.mimetype === "image/png"){
-            callback(null, true );
-        }else{
+    fileFilter: (_, file: any, callback: FileFilterCallback) => {
+        if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+            callback(null, true);
+        } else {
             callback(new Error("이미지가 아닙니다."))
         }
-    }   
+    }
 })
+
+const uploadSubImage = async (req: Request, res: Response) => {
+    const sub: Sub = res.locals.sub;
+    try {
+        const type = req.body.type;
+        // 파일의 유형을 지정치 않았을 시에는 업로드 된 파일 삭제
+        if (type !== "image" && type !== "banner") {
+            if (!req.file?.path) {
+                return res.status(400).json({ error: "유효하지 않은 파일" })
+            }
+
+            // 파일 지워주기
+            unlinkSync(req.file.path);
+            return res.status(400).json({ error: "잘못된 유형" })
+        }
+        let oldImageUrn: string = "";
+        if (type === "image") {
+            //사용중인 Urn을 저장합니다. (이전 파일을 아래서 삭제하기 위해서 )
+            oldImageUrn = sub.imageUrn || "";
+            //새로운 파일 이름을 Urn으로 넣어줍니다.
+            sub.imageUrn = req.file?.filename || "";
+        } else if (type === "banner") {
+            oldImageUrn = sub.bannerUrn || "";
+            sub.bannerUrn = req.file?.filename || "";
+        }
+        await sub.save();
+
+        //사용하지 않는 이미지 파일은 삭제 
+        if (oldImageUrn !== "") {
+            const fullFilename = path.resolve(
+                process.cwd(),
+                "public",
+                "images",
+                oldImageUrn
+            );
+            unlinkSync(fullFilename);
+        }
+        return res.json(sub);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Something went wrong" })
+    }
+}
+
 
 router.get("/:name", userMiddleware, getSub)
 router.post("/", userMiddleware, authMiddleware, createSub);
 router.get("/sub/topSubs", topSubs);
-router.post("/:name/upload", userMiddleware, authMiddleware,  ownSub, upload.single("file"), uploadSubImage)
+router.post("/:name/upload", userMiddleware, authMiddleware, ownSub, upload.single("file"), uploadSubImage)
 
 export default router;
